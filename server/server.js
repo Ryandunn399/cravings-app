654e0caddc8025063ef7a92ed456f4bfa74f4de9
Ryandunn399 <ryandunn399@gmail.com>
2023-02-16 17:35:15 -0500

    Fixed typo

--- server/server.js
+++ server/server.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 
 // Listen on port 3000
 app.listen(port, () => {
-    console.log('Example app listening on port ${port}')
+    console.log(`Example app listening on port ${port}`)
 })
