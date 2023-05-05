6d029e74f7023a3cbb8d6eb38add7f87bb22508f
Ryandunn399 <ryandunn399@gmail.com>
2023-05-05 14:06:01 -0400

    Added full statement coverage with backend functions for the client

--- client/src/utilities/__tests__/UserUtilities.test.ts
+++ client/src/utilities/__tests__/UserUtilities.test.ts
@@ -1,21 +1,46 @@
-import { callCreateUser, addUserIntolerance } from '../UserUtilities';
+/**
+ * These series of tests provide full statement coverage for the UserUtilites functions.
+ * However, due to issues with promise resolution in jest it's impossible to fully test
+ * system testing using these functions and can only rely on additional server related testing.
+ */
+import { createUserAccount, deleteUserAccount, verifyUserExists, retrieveUserAccount } from '../UserUtilities'
 
-//Tests for creating a new user
-describe("Creating a new user", () => {
+const user = 'johndoe'
+const pass = 'password123'
+const nonUser = '12345678987654321'
 
-	test("Calling callCreateUser", async() => {
-		const user = await callCreateUser("johndoe");
-		expect(user).toEqual("johndoe");
-	})
+/**
+ * Will fail the first time tests are run if the user doesn't already
+ * exist in the mongo database.
+ */
+test('Test creation of user that already exists', async () => {
+    const expectedString = 'failed'
 
-	test("Attempting to change username", async() => {
-		const user = await callCreateUser("johndoe");
-		expect(user).toEqual("johndoe");
-	})
+    return createUserAccount(user, pass).then(data => {
+        expect(data.status).toBe(expectedString)
+    })
+})
 
-	test("Making a duplicate username", async() => {
-		const user = await callCreateUser("johndoe");
-		expect(user).toEqual("johndoe");
-	})
+test('Deletion of a user account that does not exist', async () => {
+    const expectedString = 'failed'
 
+    return deleteUserAccount(nonUser).then(data => {
+        expect(data.status).toBe(expectedString)
+    })
+})
+
+test('Verify user that does not exist', async () => {
+    const expectedString = false
+
+    return verifyUserExists(nonUser).then(data => {
+        expect(data).toBe(expectedString)
+    })
+})
+
+test('Attempting to retrieve an account that does not exist', async () => {
+    const expectedString = 'failed'
+
+    return retrieveUserAccount(nonUser, pass).then(data => {
+        expect(data.status).toBe(expectedString);
+    })
 })
